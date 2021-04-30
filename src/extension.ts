3b67b8a29e73974de677d40ea5b4399dc3f4d5b3
李博修 <liboxiu@dreamofalice.club>
2021-04-30 17:41:43 +0800

    [feat]get extended markdown-it

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as MarkdownIt from 'markdown-it';
+
+
+let gmd: MarkdownIt;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,13 +19,36 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('motwoext.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
-
+		console.log(gmd);
+		console.log(gmd.render(vscode.window.activeTextEditor?.document.getText() as string, { upload: true }));
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from MotwoExt!');
 	});
 
 	context.subscriptions.push(disposable);
+	return {
+		extendMarkdownIt(md: MarkdownIt) {
+			gmd = md;
+			const defaultRender = md.renderer.rules.image;
+			gmd.renderer.rules.image = function (tokens, idx, options, env, self) {
+				var token = tokens[idx],
+					aIndex = token.attrIndex('src');
+				if (token.attrs && !token.attrs[aIndex][1].startsWith('http') && env.upload) {
+					console.log(token.attrs[aIndex][1]);
+					token.attrs[aIndex][1] = token.attrs[aIndex][1];
+				}
+
+				// pass token to default renderer.
+				if (defaultRender) {
+					return defaultRender(tokens, idx, options, env, self);
+				}
+				return "";
+
+			};
+			return md;
+		}
+	};
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
